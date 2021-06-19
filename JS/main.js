c9826840bf07c8bcbf2182d775fc3d5dab0144d9
Renad-Ahmad <rena.34end@gmail.com>
2021-06-20 01:45:27 +0300

    function to add art images

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,29 @@
-// function showPreviewOne(event){
-//     if(event.target.files.length > 0){
-//       let src = URL.createObjectURL(event.target.files[0]);
-//       let preview = document.getElementById("file-ip-1-preview");
-//       preview.src = src;
-//       preview.style.display = "block";
-//     } 
-//   }
-//   function myImgRemoveFunctionOne() {
-//     document.getElementById("file-ip-1-preview").src = "https://i.ibb.co/ZVFsg37/default.png";
-//   }
+const allTmages = document.getElementsByClassName("all_images");
+
+function add() {
+    let file = document.querySelector("input[type=file]");
+
+    for (let i = 0; i < file.files.length; i++) {
+        let e = file.files[i];
+        let div = document.createElement("div");
+        var image = document.createElement("img");
+        image.setAttribute("height", "400");
+        image.setAttribute("width", "400");
+
+        let reader = new FileReader();
+
+        reader.onloadend = function () {
+            image.src = reader.result;
+        };
+
+        if (e) {
+            reader.readAsDataURL(e);
+        } else {
+            image.src = "";
+        }
+        div.appendChild(image);
+        allTmages[i].appendChild(div);
+    }
+    console.log(image);
+}
 
