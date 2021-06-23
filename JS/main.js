09a3808f226a56a6e064c015d00f04aae8a2ce68
Renad-Ahmad <rena.34end@gmail.com>
2021-06-24 00:51:00 +0300

    add time

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,15 @@
 const allTmages = document.getElementsByClassName("all_images");
 
 function add() {
+    const date = Date().slice(0,21);
     let file = document.querySelector("input[type=file]");
 
     for (let i = 0; i < file.files.length; i++) {
         let e = file.files[i];
         let div = document.createElement("div");
         var image = document.createElement("img");
+        let time = document.createElement("small");
+
         image.setAttribute("height", "400");
         image.setAttribute("width", "400");
 
@@ -21,7 +24,10 @@ function add() {
         } else {
             image.src = "";
         }
+        time.innerHTML = date + "\n"; 
+
         div.appendChild(image);
+        div.appendChild(time); 
         allTmages[i].appendChild(div);
     }
     console.log(image);
